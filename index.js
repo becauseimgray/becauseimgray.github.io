c71501cdae1ac276a118c1df651b741f3f065501
becauseimgray <otakugray@openmailbox.org>
2017-02-20 22:26:31 -0700

    Added background images on hover for repo section

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 (function(window, document, undefined){
+  // fade out
+function fadeOut(el){
+  el.style.opacity = 0.2;
+
+  (function fade() {
+    if ((el.style.opacity -= .03) < 0) { //.1
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+// fade in
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .01) > 0.2)) { //0.1
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+
 window.onload = init;
 
   function init(){
     var invaders_link = document.getElementById('invaders');
     var block_link = document.getElementById('blockbreaker');
     var pong_link = document.getElementById('pong');
+
+
+    var invadersimg = document.getElementById('invadersimg');
+    var blockbreakerimg = document.getElementById('blockbreakerimg');
+    var pongimg = document.getElementById('pongimg');
+
     var body = document.body;
     //var background = document.getElementById('background');
 
@@ -12,51 +46,22 @@ window.onload = init;
     var breaker_image = "url('images/blockbreaker_screenie.png')";
     var pong_image = "url('images/pongl2d_screenie.png')";
 
-    invaders_link.onmouseenter = function(){
+    invaders_link.onmouseenter = function()    {fadeIn(invadersimg);};
+    block_link.onmouseenter    = function()    {fadeIn(blockbreakerimg);};
+    pong_link.onmouseenter     = function()    {fadeIn(pongimg);};
 
-        body.style.backgroundImage = invaders_image;
-    };
 
-    block_link.onmouseenter = function()   {
+    invaders_link.onmouseleave = function()    {fadeOut(invadersimg);};
+    block_link.onmouseleave    = function()    {fadeOut(blockbreakerimg);};
+    pong_link.onmouseleave     = function()    {fadeOut(pongimg);};
 
-        body.style.backgroundImage = breaker_image;
-    };
-
-    pong_link.onmouseenter = function()    {
-
-        body.style.backgroundImage = pong_image;
-    };
-    invaders_link.onmouseleave = function() {
-      background.style.opacity = 0.5;
+    document.getElementById('repos').onmouseleave = function() {
+      fadeOut(invadersimg);
+      fadeOut(blockbreakerimg);
+      fadeOut(pongimg);
     }
-  }
 
-  var fadeEffect=function(){
-  	return{
-  		init:function(id, flag, target){
-  			this.elem = document.getElementById(id);
-  			clearInterval(this.elem.si);
-  			this.target = target ? target : flag ? 100 : 0;
-  			this.flag = flag || -1;
-  			this.alpha = this.elem.style.opacity ? parseFloat(this.elem.style.opacity) * 100 : 0;
-  			this.elem.si = setInterval(function(){fadeEffect.tween()}, 20);
-  		},
-  		tween:function(){
-  			if(this.alpha == this.target){
-  				clearInterval(this.elem.si);
-  			}else{
-  				var value = Math.round(this.alpha + ((this.target - this.alpha) * .05)) + (1 * this.flag);
-  				this.elem.style.opacity = value / 100;
-  				this.elem.style.filter = 'alpha(opacity=' + value + ')';
-  				this.alpha = value
-  			}
-  		}
-  	}
-  }();
-
-
-
-
+  };
 
 
 })(window, document, undefined);
